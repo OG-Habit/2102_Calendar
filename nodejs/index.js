e98261ba2932e752505837eddfa4b700d8d945f0
OG-Habit <ivanwoogue2@gmail.com>
2021-11-15 15:51:55 +0800

    database

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,12 +1,12 @@
 var cors = require("cors");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(cors());
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -15,7 +15,6 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
-
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 })
