e259232d08f1c126f9fe061d8760a195127ecf43
OG-Habit <ivanwoogue2@gmail.com>
2021-11-23 19:50:45 +0800

    Merge branch 'main' of https://github.com/ydblank/2102_Calendar into ydblank-main

--- reactjs/src/components/Reminder.js
+++ reactjs/src/components/Reminder.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react'
 import './Reminder.css';
+class Reminder extends Component {
+    static num;
+    constructor(props){
+        super(props);
+        this.day=props.day;
+        this.month=props.month;
+        this.year=props.year;  
+        this.weekDay=new Date(props.year, props.month, props.day).getDay();
+        this.monthStr=[
+            "JAN.",
+            "FEB.",
+            "MAR.",
+            "APR.",
+            "MAY",
+            "JUN.",
+            "JUL.",
+            "AUG.",
+            "SEP.",
+            "OCT.",
+            "NOV.",
+            "DEC."
+        ]
+    }
 
-class Sample extends Component {
     render() {
         return (
             <div className="Reminder">
-                {/* Reminder component here */}
+                <p><h1>{this.weekDay}</h1></p>
+                <h1>{this.monthStr[this.month]}{this.day},{this.year}</h1>
             </div>
         )
     }
 }
 
-export default Sample
+export default Reminder
