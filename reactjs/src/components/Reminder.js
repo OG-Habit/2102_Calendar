5334da92328d674ca625667b04a34345e14ffb6d
yd_blank <email@example.com>
2021-11-24 10:15:05 +0800

    Reminder input

--- reactjs/src/components/Reminder.js
+++ reactjs/src/components/Reminder.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react'
 import './Reminder.css';
+import {setState} from "react";
 class Reminder extends Component {
     static num;
     constructor(props){
         super(props);
-        this.day=props.day;
-        this.month=props.month;
-        this.year=props.year;  
+        this.day=1;
+        this.month=2;
+        this.year=2002;  
         this.weekDay=new Date(props.year, props.month, props.day).getDay();
         this.monthStr=[
-            "JAN.",
-            "FEB.",
-            "MAR.",
-            "APR.",
-            "MAY",
-            "JUN.",
-            "JUL.",
-            "AUG.",
-            "SEP.",
-            "OCT.",
-            "NOV.",
-            "DEC."
+            "Jan.",
+            "Feb.",
+            "Mar.",
+            "Apr.",
+            "May",
+            "Jun.",
+            "Jul.",
+            "Aug.",
+            "Sep.",
+            "Oct.",
+            "Nov.",
+            "Dec."
         ]
+        this.state={
+            reminder:"",
+        }
     }
+
+    setReminder=(event)=>{
+        this.setState({reminder:event.target.value})   
+    }
+
+    displayReminder=()=>{
+        alert(this.state.reminder)
+    }
+    
 
     render() {
         return (
             <div className="Reminder">
+                <br></br><br></br>
                 <p><h1>{this.weekDay}</h1></p>
-                <h1>{this.monthStr[this.month]}{this.day},{this.year}</h1>
+                <h1>{this.monthStr[this.month]} {this.day}, {this.year}</h1>
+                <br></br>
+                <h3>Reminders:</h3>
+                <input type="text" placeholder="New reminder" onChange={this.setReminder}/>
+                <div id="cross" onClick={this.displayReminder}></div>
+                
             </div>
         )
     }
