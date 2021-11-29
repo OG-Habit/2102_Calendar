b6ea88863211a8d53fc71696b414e2965e4368f0
OG-Habit <ivanwoogue2@gmail.com>
2021-11-29 14:27:11 +0800

    uncomment

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
   import App from './App';
-// import AccountSetup from './components/AccountSetup';
+import AccountSetup from './components/AccountSetup';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
