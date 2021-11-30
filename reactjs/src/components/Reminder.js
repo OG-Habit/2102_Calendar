191468816b7e03f4a03aab40421229a51079a855
yd_blank <email@example.com>
2021-11-30 15:41:40 +0800

    reminder list and modal

--- reactjs/src/components/Reminder.js
+++ reactjs/src/components/Reminder.js
@@ -36,8 +36,10 @@ class Reminder extends Component {
         alert(this.state.reminder)
     }
     
+    state = { showing: true }
 
     render() {
+        const { showing } = this.state;
         return (
             <div className="Reminder">
                 <br></br><br></br>
@@ -45,12 +47,72 @@ class Reminder extends Component {
                 <h1>{this.monthStr[this.month]} {this.day}, {this.year}</h1>
                 <br></br>
                 <h3>Reminders:</h3>
-                <input type="text" placeholder="New reminder" onChange={this.setReminder}/>
-                <div id="cross" onClick={this.displayReminder}></div>
-                
+                <table id="reminder-list">
+                    <col span="1" style={{width: "90%"}}/>
+                    <col span="1" style={{width: "10%"}}/>
+                    <tr>
+                        <td>
+                            Sample Reminder no.1
+                        </td>
+                        <td>
+                        <button type="button" class="btn btn-primary btn-sm" onClick={() => this.setState({ showing: !showing })}>Edit</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Sample Reminder no.2
+                        </td>
+                        <td>
+                        <button type="button" class="btn btn-primary btn-sm" onClick={() => this.setState({ showing: !showing })}>Edit</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Sample Reminder no.3
+                        </td>
+                        <td>
+                        <button type="button" class="btn btn-primary btn-sm" onClick={() => this.setState({ showing: !showing })}>Edit</button>
+                        </td>
+                    </tr>
+                </table>
+                <p>{this.state.reminder}</p>
+
+                <button type="button" class="btn btn-primary btn-sm" onClick={() => this.setState({ showing: !showing })}>Add</button>
+
+                <div class="container" id="container" style={{ display: (showing ? 'block' : 'none') }}>
+                    <label for="show" class="close" onClick={() => this.setState({ showing: !showing })}>x</label>
+                    <div class="text">
+                        {this.monthStr[this.month]} {this.day}, {this.year}
+                    </div>
+                    <form action ="" id = "form" onsubmit="" method = "POST">
+                        <div class="data">
+                            <label>Reminder:</label>
+                            <input type="text" name="firstName" id="firstName" require/>
+                        </div>
+                        <div class="data">
+                            <label>Description</label>
+                            <textarea type="text" name= "lastName" id="lastName" require/>
+                        </div>
+                        <div class="data-time">
+                            <label>Time Start</label>
+                            <input type="time"/>
+                        </div>
+                        <div class="data-time">
+                            <label>Time End</label>
+                            <input type="time"/>
+                        </div>
+                            <input type="text" hidden name= "id" id="id" require/>                                <input hidden name="edit-id" id="edit-id"/>
+                        <div class="btn">
+                            <input type="submit" name="btn" id="btn" value="Add"></input>
+                        </div>
+                    </form>
+                </div>
             </div>
+            
         )
     }
 }
+
+
 
 export default Reminder
