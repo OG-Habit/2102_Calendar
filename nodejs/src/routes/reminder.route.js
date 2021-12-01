86798376d94e6567d8c232a1bd400ae7ec308705
Frooge <jaantaros@gmail.com>
2021-12-01 16:26:57 +0800

    Changed reminder route

--- nodejs/src/routes/reminder.route.js
+++ nodejs/src/routes/reminder.route.js
@@ -4,7 +4,7 @@ const reminderController = require('../controllers/reminder.controller');
 
 router.get('/:user_id', reminderController.findByUserID);
 router.post('/', reminderController.create);
-router.put('/:rem_id', reminderController.update);
-router.patch('/:rem_id', reminderController.delete);
+router.post('/:rem_id', reminderController.update);
+router.post('/delete/:rem_id', reminderController.delete);
 
 module.exports = router;
