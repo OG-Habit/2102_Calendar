df70ce7b50bf390749437341c5ce1178813202c9
Frooge <jaantaros@gmail.com>
2021-12-11 16:19:31 +0800

    Added change password toggle on profile

--- reactjs/src/components/UserProfile.js
+++ reactjs/src/components/UserProfile.js
@@ -19,6 +19,7 @@ export default class UserProfile extends Component {
             user: [],
             loadUser: true,
             mode: 0,
+            changePass: 0,
             icon: 'icon1',
         }
     }
@@ -45,14 +46,23 @@ export default class UserProfile extends Component {
 
     updateUser = () => {
         let value = $('#userform').serializeArray(),
-        obj = {};
+        obj = {},
+        valid = true;
         $(value).each(function(i, field){
             obj[field.name] = field.value;
         });
-        if(obj["old-password"] !== this.state.user.password) {
-            alert("Incorrect old password");
+
+        if(this.state.changePass){
+            if(obj["old-password"] !== this.state.user.password) {
+                alert("Incorrect old password");
+                valid = false;
+            }
         }
         else {
+            obj["new-password"] = this.state.user.password;
+        }
+
+        if(valid) {
             let data = {
                 fname: obj["firstName"],
                 lname: obj["lastName"],
@@ -82,14 +92,25 @@ export default class UserProfile extends Component {
         this.setState({
             icon: icon
         })
+        this.setMode();
+    }
+
+    setChangePass = () => {
+        this.setState({
+            changePass: (this.state.changePass+1)%2
+        })
     }
 
     render() {
         return this.state.loadUser ?
         (
-            <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </Spinner>
+        <div className="profile-container" style={{display: this.props.show ? 'display' : 'none'}}>
+            <div className="row justify-content-center">
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </div>
+        </div>
         ) : (
         <div className="profile-container" style={{display: this.props.show ? 'display' : 'none'}}>
           <button className="close" onClick={() => this.props.setShow(false)}>x</button>
@@ -105,30 +126,62 @@ export default class UserProfile extends Component {
             <div className="profile-form-container" style={{display: this.state.mode === 0 ? 'block' : 'none'}}>
                 <div className="row mb-3">
                     <label className="col-sm-3">First Name:</label>
-                    <input type="text" name="firstName" className="form-control col" defaultValue={this.state.user.firstname} required/>
+                    <input type="text" name="firstName" className="form-control col"
+                    defaultValue={this.state.user.firstname} required/>
                 </div>
                 <div className="row mb-3">
                     <label className="col-sm-3">Last Name:</label>
-                    <input type="text" name="lastName" className="form-control col" defaultValue={this.state.user.lastname} required/>
+                    <input type="text" name="lastName" className="form-control col"
+                    defaultValue={this.state.user.lastname} required/>
+                    
                 </div>
-                <div className="row mb-3">
-                    <label className="col-sm-3">Password: (Current)</label>
-                    <input type="password" name="new-password" className="form-control col" defaultValue={this.state.user.password} required/>
-                </div>
-                <div className="row mb-3">
+                <div className="row mb-2">
                     <label className="col-sm-3">Old Password:</label>
-                    <input type="password" name="old-password" className="form-control col" required/>
+                    <div className="col-sm-5 align-self-start">
+                        <input type="password" name="old-password" className="form-control col"
+                        readOnly={!this.state.changePass ? true : false} required/>
+                    </div>
+                    <div className="form-check col-sm-4">
+                        <input class="form-check-input" type="checkbox" onClick={this.setChangePass}/>
+                        <label class="form-check-label">
+                            Change Password
+                        </label>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <label className="col-sm-3">New Password:</label>
+                    
+                    <div className="col-sm-5 align-self-start">
+                        <input type="password" name="new-password" className="form-control"
+                        readOnly={!this.state.changePass ? true : false} required/>
+                    </div>
                 </div>
             </div>
             <div className="avatar-pick-grid" style={{display: this.state.mode === 1 ? 'block' : 'none'}}>
-                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon1')}><img src={icon1} alt="avatar1.png"></img></button>
-                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon2')}><img src={icon2} alt="avatar2.png"></img></button>
-                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon3')}><img src={icon3} alt="avatar3.png"></img></button>
-                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon4')}><img src={icon4} alt="avatar4.png"></img></button>
-                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon5')}><img src={icon5} alt="avatar5.png"></img></button>
-                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon6')}><img src={icon6} alt="avatar6.png"></img></button>
-                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon7')}><img src={icon7} alt="avatar7.png"></img></button>
-                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon8')}><img src={icon8} alt="avatar8.png"></img></button>
+                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon1')}>
+                    <img src={icon1} alt="avatar1.png"></img>
+                </button>
+                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon2')}>
+                    <img src={icon2} alt="avatar2.png"></img>
+                </button>
+                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon3')}>
+                    <img src={icon3} alt="avatar3.png"></img>
+                </button>
+                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon4')}>
+                    <img src={icon4} alt="avatar4.png"></img>
+                </button>
+                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon5')}>
+                    <img src={icon5} alt="avatar5.png"></img>
+                </button>
+                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon6')}>
+                    <img src={icon6} alt="avatar6.png"></img>
+                </button>
+                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon7')}>
+                    <img src={icon7} alt="avatar7.png"></img>
+                </button>
+                <button type="button" className="avatar-button" onClick={() => this.setIcon('icon8')}>
+                    <img src={icon8} alt="avatar8.png"></img>
+                </button>
             </div>
             <button type="submit" className="btn btn-success btn-sm" id="apply-profile-btn">Apply Changes</button>
           </form>
